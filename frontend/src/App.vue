27a22b3864a067fb28c2e17602f8afca38a01230
filipeodev <filipeodev@gmail.com>
2023-01-10 08:26:01 -0300

    Options api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,49 +16,26 @@
   
 </template>
 
-<script setup> //setup é para composition api
+<script>
 // options api -> versão 2 do vue
 
-// import Header from '@/components/Header.vue';
-// import Footer from '@/components/Footer.vue';
-
-// export default {
-//   components:{Header,Footer},
-    //tudo  ue está dentro do data é reativo (chamado reatividade)
-//   data() {
-//     return {
-//       count:0
-//     }
-//   },
-//   methods: {
-//     add(){
-//       this.count++
-//     }
-//   }
-// }
-
-
-// composition api -> novidade do vue 3 ==--------------------------------------
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
-import {ref} from 'vue';
-// export default { //posso comentar se tiver setup na tag script
-  // components:{Header,Footer}, //posso comentar se tiver setup na tag script
-  // setup(){ //posso comentar se tiver setup na tag script
-    //para ter reatividade (dados reativos) usa-se ref ou reactive (ref para dados primitivos)
-    const count = ref(0);
 
-    function add(){
-      count.value++;
+export default {
+  components:{Header,Footer},
+    // tudo  ue está dentro do data é reativo (chamado reatividade)
+  data() {
+    return {
+      count:0
     }
-
-    // return { //posso comentar se tiver setup na tag script
-    //   count, add
-    // }
-  // } //posso comentar se tiver setup na tag script
-// } //posso comentar se tiver setup na tag script
-
-
+  },
+  methods: {
+    add(){
+      this.count++
+    }
+  }
+}
 </script>
 
 <style scoped>
