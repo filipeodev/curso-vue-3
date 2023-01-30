30683ccf92d8a5aab88e2c6949ec3f0cc917298a
filipeodev <filipeodev@gmail.com>
2023-01-30 10:31:32 -0300

    trabalhando com propriedades reativas options-api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
 <template>
 
+  <ul>
+    <li v-for="user in users">{{ user.firstName }} - {{ user.age }}</li>
+  </ul>
+
   <div v-if="showHeader">
     <Header />
   </div>
+  <br>
 
   <button v-on:click="showHeader = !showHeader">Toogle Header</button>
+  <br>
+  <br>
 
   <h2>App</h2>
+  <br>
+  <br>
   
   <router-link to="/">Home</router-link>
   <router-link to="/about">Avout</router-link>
@@ -22,11 +31,22 @@ export default {
   data(){
     return{
       count:0,
-      showHeader:false
+      showHeader:false,
+      userName:"Filipe",
+      users:[
+        {
+          firstName:"Filipe",
+          age: 24
+        },
+        {
+          firstName:"Teste",
+          age: 24
+        }
+      ]
     }
   },
   mounted(){
-    console.log('mounted');
+    console.log(this.count);
   },
   updated(){
     console.log('updated');
