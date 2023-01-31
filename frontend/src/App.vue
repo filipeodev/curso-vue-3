a4673cdde0cf770cb40788087c15c010a4ed1c75
filipeodev <filipeodev@gmail.com>
2023-01-31 10:43:10 -0300

    Listando dados Backend options API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,66 +1,35 @@
 <template>
-  <Header />
-
-  <br/>
-  
-  <!-- <button v-on:click="add()">Add {{ count }}</button> -->
-  <button v-on:click="add">Add {{ count }}</button>
-
-  <br/>
-
-  <h2 id="my-app">App</h2>
-
-  <br/>
-
-  <Footer />
-  
+  <h2>Lista de users</h2>
+  <ul>
+    <li v-for="user in users" :key="user.id">{{ user.firstName }}</li>
+  </ul>
 </template>
 
-<script setup> //setup é para composition api
-// options api -> versão 2 do vue
+<script>
+import http from '@/services/http.js';
+// import axios from 'axios';
 
-// import Header from '@/components/Header.vue';
-// import Footer from '@/components/Footer.vue';
-
-// export default {
-//   components:{Header,Footer},
-    //tudo  ue está dentro do data é reativo (chamado reatividade)
-//   data() {
-//     return {
-//       count:0
-//     }
-//   },
-//   methods: {
-//     add(){
-//       this.count++
-//     }
-//   }
-// }
-
-
-// composition api -> novidade do vue 3 ==--------------------------------------
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
-import {ref} from 'vue';
-// export default { //posso comentar se tiver setup na tag script
-  // components:{Header,Footer}, //posso comentar se tiver setup na tag script
-  // setup(){ //posso comentar se tiver setup na tag script
-    //para ter reatividade (dados reativos) usa-se ref ou reactive (ref para dados primitivos)
-    const count = ref(0);
-
-    function add(){
-      count.value++;
+export default {
+  data(){
+    return {
+      users:[]
     }
+  },
 
-    // return { //posso comentar se tiver setup na tag script
-    //   count, add
-    // }
-  // } //posso comentar se tiver setup na tag script
-// } //posso comentar se tiver setup na tag script
+  async mounted() {
+    try {
+      const {data} = await http.get('http://localhost:8000/api/users');
+      this.users = data;
+    }catch(error){
+      console.log(error);
+    }
+  },
 
-
+  updated() {
+    console.log('updated');
+  }
+}
 </script>
 
 <style scoped>
-@import "@/assets/app.css";
 </style>
