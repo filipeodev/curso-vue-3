72dfd9216d06f31135fdc9dcdbbed9a0249b5576
filipeodev <filipeodev@gmail.com>
2023-02-01 10:55:16 -0300

    Renderização condicional

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,39 @@
 <template>
+  <template v-show="showHeader">
+    <Header />
+  </template>
+  
+  <!-- <template v-if="showHeader">
+    <Header />
+  </template>
+  <span v-else-if="showElseIf">Nada</span>
+  <span v-else>
+      Sem Header
+  </span> -->
+  <!-- <Header v-if="showHeader" /> -->
+  <hr>
   <h2>Lista de users</h2>
   <ul>
-    <li v-for="user in users" :key="user.id">{{ user.firstName }}</li>
+    <li v-for="user in users" :key="user.id">
+      {{ user.firstName }} 
+      <button v-if="user.is_admin">Admin</button>
+      <button v-else>Not Admin</button>
+    </li>
   </ul>
 </template>
 
 <script>
 import http from '@/services/http.js';
+import Header from '@/components/Header.vue';
 // import axios from 'axios';
 
 export default {
+  components:{Header},
   data(){
     return {
-      users:[]
+      users:[],
+      showHeader:false,
+      showElseIf:true
     }
   },
 
