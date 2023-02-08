b639b8f490751ebb74a654637b86e632b3ed0131
filipeodev <filipeodev@gmail.com>
2023-02-08 10:59:05 -0300

    18. Vue 3 para iniciantes#18 - Diretiva v-bind e classes com condição

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,8 @@
 <template>
 
+  <img :src="imageSrc" alt="imagem" class="my-default-class" :class="{'my-class':is_admin, 'my-other-class':!is_admin}">
+  <hr>
+
   <h2>Lista de users</h2>
 
   <ul>
@@ -10,9 +13,11 @@
 
 <script setup>
 import http from '@/services/http.js';
-import {onMounted, reactive} from "vue";
+import {onMounted, reactive, ref} from "vue";
 
 let users = reactive({users:[]});
+const  imageSrc = ref('https://picsum.photos/200/300');
+const is_admin = ref(false);
 
 onMounted(async () => {
   try {
@@ -26,4 +31,12 @@ onMounted(async () => {
 
 <style scoped>
 @import "@/assets/app.css";
+
+.my-class {
+  border: 3px solid red;
+}
+
+.my-other-class {
+  border: 3px solid blue;
+}
 </style>
