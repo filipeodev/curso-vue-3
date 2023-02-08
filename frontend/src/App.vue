c48839cf9108d94967a28e25fa77eeffa3512425
filipeodev <filipeodev@gmail.com>
2023-02-08 11:17:38 -0300

    19. Vue 3 para iniciantes#19 - Eventos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 
   <img :src="imageSrc" alt="imagem" class="my-default-class" :class="{'my-class':is_admin, 'my-other-class':!is_admin}">
+  <button @click="add(teste)">Click aqui {{ count }}</button>
   <hr>
 
   <h2>Lista de users</h2>
@@ -18,6 +19,12 @@ import {onMounted, reactive, ref} from "vue";
 let users = reactive({users:[]});
 const  imageSrc = ref('https://picsum.photos/200/300');
 const is_admin = ref(false);
+const count = ref(0);
+
+function add(teste) {
+  count.value++;
+  console.log(teste);
+}
 
 onMounted(async () => {
   try {
